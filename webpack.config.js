5ee756b4f71915452c6215c4652fb6a84b99900e
Sara Orsi <sararaorsi@gmail.com>
2022-10-01 12:04:34 +0100

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  watch: true,
+  entry: {
+    app: ["./assets/scss/styles.scss", "./assets/js/app.js"],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
+};
